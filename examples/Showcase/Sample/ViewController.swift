9b50835bcf6c8711e75d2a48cc4bd4f0d646e954
Roy Marmelstein <marmelroy@gmail.com>
2015-11-25 22:30:57 +0100

    Initial implementation of example

--- examples/Showcase/Sample/ViewController.swift
+++ examples/Showcase/Sample/ViewController.swift
@@ -2,71 +2,60 @@
 //  ViewController.swift
 //  Sample
 //
-//  Created by Roy Marmelstein on 05/08/2015.
+//  Created by Roy Marmelstein on 25/11/2015.
 //  Copyright (c) 2015 Roy Marmelstein. All rights reserved.
 //
 
 import UIKit
-import Localize_Swift
+import Format
 
 class ViewController: UIViewController {
+    
+    var randomNumber: CGFloat = CGFloat(arc4random_uniform(99) + 1) + CGFloat(arc4random_uniform(99) + 1)/100.00
+    
+    @IBOutlet weak var originalLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
 
-    @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var changeButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    
-    var actionSheet: UIAlertController!
-    
-    let availableLanguages = Localize.availableLanguages()
-    
     // MARK: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setText()
+        originalLabel.text = "\(randomNumber)"
+        resultLabel.text = "\(randomNumber)"
     }
     
-    // Add an observer for LCLLanguageChangeNotification on viewWillAppear. This is posted whenever a language changes and allows the viewcontroller to make the necessary UI updated. Very useful for places in your app when a language change might happen.
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "setText", name: LCLLanguageChangeNotification, object: nil)
     }
     
-    // Remove the LCLLanguageChangeNotification on viewWillDisappear
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
-    // MARK: Localized Text
+    // MARK: Actions
     
-    func setText(){
-        textLabel.text = "Hello world".localized();
-        changeButton.setTitle("Change".localized(), forState: UIControlState.Normal)
-        resetButton.setTitle("Reset".localized(), forState: UIControlState.Normal)
+    @IBAction func doApplyDecimalNone(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(Decimals.None)
     }
     
-    // MARK: IBActions
-
-    @IBAction func doChangeLanguage(sender: AnyObject) {
-        actionSheet = UIAlertController(title: nil, message: "Switch Language", preferredStyle: UIAlertControllerStyle.ActionSheet)
-        for language in availableLanguages {
-            let displayName = Localize.displayNameForLanguage(language)
-            let languageAction = UIAlertAction(title: displayName, style: .Default, handler: {
-                (alert: UIAlertAction!) -> Void in
-                    Localize.setCurrentLanguage(language)
-            })
-            actionSheet.addAction(languageAction)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {
-            (alert: UIAlertAction) -> Void in
-        })
-        actionSheet.addAction(cancelAction)
-        self.presentViewController(actionSheet, animated: true, completion: nil)
+    @IBAction func doApplyDecimalThree(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(Decimals.Three)
     }
-
-    @IBAction func doResetLanguage(sender: AnyObject) {
-        Localize.resetCurrentLanguageToDefault()
+    
+    @IBAction func doApplyCurrencyUSD(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(Currency.USD)
+    }
+    
+    @IBAction func doApplyGeneralOrdinal(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(General.Ordinal)
+    }
+    
+    @IBAction func doApplyGeneralSpellOut(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(General.SpellOut)
+    }
+    
+    @IBAction func doApplyGeneralDistance(sender: AnyObject) {
+        resultLabel.text = randomNumber.format(General.Distance)
     }
 }
 
