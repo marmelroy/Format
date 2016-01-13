32896e786888e44ab9f4a197fdd411bd8d09291a
John Lawrence <johnathan.lawrence@capitalone.com>
2016-01-13 14:34:33 -0500

    Revert "Fixes a compile issue with Xcode 7.0.1 and Swift 2.0"
    This reverts commit 663c82a1bac98b68079857a806e8f27de4be7ba3.

--- Format/NumberExtensions.swift
+++ Format/NumberExtensions.swift
@@ -146,7 +146,7 @@ extension Int64: NumberFormatProvider {
 
 extension UInt: NumberFormatProvider {
     public func formatNumber() -> NSNumber {
-        return NSNumber(unsignedLong: self)
+        return NSNumber(unsignedInteger: self)
     }
 }
 
